07bf6bde04b7f8f2d350375aa907755c3c109069
delaperijoshua8-ux <delaperijoshua8@gmail.com>
2026-02-05 22:26:47 +0800

    Add files via upload

---
+++ BSIT1DG1/SetA.java
@@ -0,0 +1,22 @@
+class SetA{
+
+	public static void main(String Owen[]){
+
+
+	int jn=35;
+
+
+	if (jn  % 2  == 0)
+	{
+	System.out.print("Even");
+	}
+	else
+	{
+	System.out.print("Odd");
+	}
+
+	}
+
+
+
+}
